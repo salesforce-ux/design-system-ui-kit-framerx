3718bcd2d68f1f3deb65386af58d6ed245a1f600
Darshil Vora <darshilv@gmail.com>
2020-06-15 13:28:12 -0700

    published package

---
+++ README.md
@@ -0,0 +1,1 @@
+This UI Kit is a collection of Framer components of Lightning Design System. The react component base is taken from Salesforce' Lightning Design System for React. The thousands of customers and partners who build applications on the Salesforce platform can use these resources to quickly visualize and test designs before they're implemented in their Salesforce organizations.
--- code/icon.tsx
+++ code/icon.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import { addPropertyControls, ControlType } from "framer"
 
-import { ActionIcon } from "./icon-components/action-icon-components/ActionIcon"
-import { UtilityIcon } from "./icon-components/utility-icon-components/UtilityIcon"
-import { StandardIcon } from "./icon-components/standard-icon-components/StandardIcon"
-import { DoctypeIcon } from "./icon-components/doctype-icon-components/DoctypeIcon"
-import { CustomIcon } from "./icon-components/custom-icon-components/CustomIcon"
+import { ActionIcon } from "@framer/darshilv.lightning-design-system-ui-icon-kit/code/action-icon-components/ActionIcon"
+import { UtilityIcon } from "@framer/darshilv.lightning-design-system-ui-icon-kit/code/utility-icon-components/UtilityIcon"
+import { StandardIcon } from "@framer/darshilv.lightning-design-system-ui-icon-kit/code/standard-icon-components/StandardIcon"
+import { DoctypeIcon } from "@framer/darshilv.lightning-design-system-ui-icon-kit/code/doctype-icon-components/DoctypeIcon"
+import { CustomIcon } from "@framer/darshilv.lightning-design-system-ui-icon-kit/code/custom-icon-components/CustomIcon"
 
 const style: React.CSSProperties = {
     width: "100%",
